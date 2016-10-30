37c32028725aaec57fd517f93799a266b95a61b0
Ivailo Monev <xakepa10@laimg.moc>
2016-10-30 03:28:35 +0000

    remove client and daemon sources
    Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

--- createtararchive.sh
+++ createtararchive.sh
@@ -1,13 +1,6 @@
 #! /bin/bash
 # script to create a release tarball for strigi
 # this is needed since the project is split up in separate git moduls
-TAG=0.7.6
-git archive --format=tar --prefix=strigi-$TAG/ -o strigi-$TAG.tar v$TAG
-for f in libstreams libstreamanalyzer strigidaemon strigiclient strigiutils; do
-  cd $f
-  git archive --format=tar --prefix=strigi-$TAG/$f/ -o tmptar v$TAG
-  tar Af ../strigi-$TAG.tar tmptar
-  rm tmptar
-  cd -
-done
-bzip2 -9 strigi-$TAG.tar
+TAG=0.8.0
+git archive HEAD --format=tar --prefix=strigi-$TAG/ | xz > strigi-$TAG.tar.xz
+
