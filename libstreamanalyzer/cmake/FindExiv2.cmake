ca7106135e2b4c180e0681abfcc4e30e45c43614
Ivailo Monev <xakepa10@gmail.com>
2021-01-07 10:59:46 +0200

    updated bundled Exiv2 and FFmpeg CMake modules from Katana libraries repo
    fixes their detection when in non-standard prefixes like /usr/pkg for
    pkgsrc users
    
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- libstreamanalyzer/cmake/FindExiv2.cmake
+++ libstreamanalyzer/cmake/FindExiv2.cmake
@@ -1,80 +1,44 @@
-# - Try to find the Exiv2 library
-#
-#  EXIV2_MIN_VERSION - You can set this variable to the minimum version you need
-#                      before doing FIND_PACKAGE(Exiv2). The default is 0.12.
+# - Try to find Exiv2 library
 #
 # Once done this will define
 #
-#  EXIV2_FOUND - system has libexiv2
-#  EXIV2_INCLUDE_DIR - the libexiv2 include directory
-#  EXIV2_LIBRARIES - Link these to use libexiv2
-#  EXIV2_DEFINITIONS - Compiler switches required for using libexiv2
+#  EXIV2_FOUND - system has Exiv2
+#  EXIV2_INCLUDE_DIR - the Exiv2 include directory
+#  EXIV2_LIBRARIES - the libraries needed to use Exiv2
+#  EXIV2_DEFINITIONS - compiler switches required for using Exiv2
 #
-# The minimum required version of Exiv2 can be specified using the
-# standard syntax, e.g. find_package(Exiv2 0.17)
-#
-# For compatiblity, also the variable EXIV2_MIN_VERSION can be set to the minimum version
-# you need before doing FIND_PACKAGE(Exiv2). The default is 0.12.
-
-# Copyright (c) 2010, Alexander Neundorf, <neundorf@kde.org>
-# Copyright (c) 2008, Gilles Caulier, <caulier.gilles@gmail.com>
+# Copyright (c) 2020, Ivailo Monev, <xakepa10@gmail.com>
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
-# Support EXIV2_MIN_VERSION for compatibility:
-if(NOT Exiv2_FIND_VERSION)
-  set(Exiv2_FIND_VERSION "${EXIV2_MIN_VERSION}")
-endif(NOT Exiv2_FIND_VERSION)
+if(NOT WIN32)
+    include(FindPkgConfig)
+    pkg_check_modules(PC_EXIV2 QUIET exiv2)
 
-# the minimum version of exiv2 we require
-if(NOT Exiv2_FIND_VERSION)
-  set(Exiv2_FIND_VERSION "0.12")
-endif(NOT Exiv2_FIND_VERSION)
+    set(EXIV2_INCLUDE_DIR ${PC_EXIV2_INCLUDE_DIRS})
+    set(EXIV2_LIBRARIES ${PC_EXIV2_LIBRARIES})
+endif()
 
+set(EXIV2_VERSION ${PC_EXIV2_VERSION})
+set(EXIV2_DEFINITIONS ${PC_EXIV2_CFLAGS_OTHER})
 
-if (NOT WIN32)
-   # use pkg-config to get the directories and then use these values
-   # in the FIND_PATH() and FIND_LIBRARY() calls
-   find_package(PkgConfig)
-   pkg_check_modules(PC_EXIV2 exiv2)
-   set(EXIV2_DEFINITIONS ${PC_EXIV2_CFLAGS_OTHER})
-endif (NOT WIN32)
+if(NOT EXIV2_INCLUDE_DIR OR NOT EXIV2_LIBRARIES)
+    find_path(EXIV2_INCLUDE_DIR
+        NAMES exiv2/exif.hpp
+        HINTS $ENV{EXIV2DIR}/include
+    )
 
-
-find_path(EXIV2_INCLUDE_DIR NAMES exiv2/exif.hpp
-          HINTS
-          ${PC_EXIV2_INCLUDEDIR}
-          ${PC_EXIV2_INCLUDE_DIRS}
-        )
-
-find_library(EXIV2_LIBRARY NAMES exiv2 libexiv2
-             HINTS
-             ${PC_EXIV2_LIBDIR}
-             ${PC_EXIV2_LIBRARY_DIRS}
-            )
-
-
-# Get the version number from exiv2/version.hpp and store it in the cache:
-if(EXIV2_INCLUDE_DIR  AND NOT  EXIV2_VERSION)
-  file(READ ${EXIV2_INCLUDE_DIR}/exiv2/version.hpp EXIV2_VERSION_CONTENT)
-  string(REGEX MATCH "#define EXIV2_MAJOR_VERSION +\\( *([0-9]+) *\\)"  _dummy "${EXIV2_VERSION_CONTENT}")
-  set(EXIV2_VERSION_MAJOR "${CMAKE_MATCH_1}")
-
-  string(REGEX MATCH "#define EXIV2_MINOR_VERSION +\\( *([0-9]+) *\\)"  _dummy "${EXIV2_VERSION_CONTENT}")
-  set(EXIV2_VERSION_MINOR "${CMAKE_MATCH_1}")
-
-  string(REGEX MATCH "#define EXIV2_PATCH_VERSION +\\( *([0-9]+) *\\)"  _dummy "${EXIV2_VERSION_CONTENT}")
-  set(EXIV2_VERSION_PATCH "${CMAKE_MATCH_1}")
-
-  set(EXIV2_VERSION "${EXIV2_VERSION_MAJOR}.${EXIV2_VERSION_MINOR}.${EXIV2_VERSION_PATCH}" CACHE STRING "Version number of Exiv2" FORCE)
-endif(EXIV2_INCLUDE_DIR  AND NOT  EXIV2_VERSION)
-
-set(EXIV2_LIBRARIES "${EXIV2_LIBRARY}")
+    find_library(EXIV2_LIBRARIES
+        NAMES exiv2
+        HINTS $ENV{EXIV2DIR}/lib
+    )
+endif()
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Exiv2  REQUIRED_VARS  EXIV2_LIBRARY EXIV2_INCLUDE_DIR
-                                         VERSION_VAR  EXIV2_VERSION)
+find_package_handle_standard_args(Exiv2
+    VERSION_VAR EXIV2_VERSION
+    REQUIRED_VARS EXIV2_LIBRARIES EXIV2_INCLUDE_DIR
+)
 
-mark_as_advanced(EXIV2_INCLUDE_DIR EXIV2_LIBRARY)
-
+mark_as_advanced(EXIV2_INCLUDE_DIR EXIV2_LIBRARIES)
