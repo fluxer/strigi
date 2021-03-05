341a7f8a9e20cfa517f00e70fd02e9f0b7c70dcb
Ivailo Monev <xakepa10@gmail.com>
2021-03-05 22:30:16 +0200

    do not rely on pkg-config for Exiv2 library lookup
    fixes build on FreeBSD
    
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- cmake/FindExiv2.cmake
+++ cmake/FindExiv2.cmake
@@ -15,25 +15,20 @@
 if(NOT WIN32)
     include(FindPkgConfig)
     pkg_check_modules(PC_EXIV2 QUIET exiv2)
-
-    set(EXIV2_INCLUDE_DIR ${PC_EXIV2_INCLUDE_DIRS})
-    set(EXIV2_LIBRARIES ${PC_EXIV2_LIBRARIES})
 endif()
 
 set(EXIV2_VERSION ${PC_EXIV2_VERSION})
 set(EXIV2_DEFINITIONS ${PC_EXIV2_CFLAGS_OTHER})
 
-if(NOT EXIV2_INCLUDE_DIR OR NOT EXIV2_LIBRARIES)
-    find_path(EXIV2_INCLUDE_DIR
-        NAMES exiv2/exif.hpp
-        HINTS $ENV{EXIV2DIR}/include
-    )
+find_path(EXIV2_INCLUDE_DIR
+    NAMES exiv2/exif.hpp
+    HINTS $ENV{EXIV2DIR}/include
+)
 
-    find_library(EXIV2_LIBRARIES
-        NAMES exiv2
-        HINTS $ENV{EXIV2DIR}/lib
-    )
-endif()
+find_library(EXIV2_LIBRARIES
+    NAMES exiv2
+    HINTS $ENV{EXIV2DIR}/lib
+)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Exiv2
