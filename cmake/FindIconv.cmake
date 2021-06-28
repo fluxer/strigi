818cbe9019952d38071ab680dc42bbf8543557a9
Ivailo Monev <xakepa10@gmail.com>
2021-06-29 01:38:20 +0300

    use macro to reset state in iconv module
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- cmake/FindIconv.cmake
+++ cmake/FindIconv.cmake
@@ -5,39 +5,41 @@
 #  ICONV_INCLUDE_DIR - the Iconv include directory 
 #  ICONV_LIBRARIES - Link these to use Iconv 
 #  ICONV_SECOND_ARGUMENT_IS_CONST - the second argument for iconv() is const
-# 
+#
+
+include(CMakePushCheckState)
 include(CheckCXXSourceCompiles)
 
-FIND_PATH(ICONV_INCLUDE_DIR iconv.h) 
+find_path(ICONV_INCLUDE_DIR iconv.h)
  
-FIND_LIBRARY(ICONV_LIBRARIES NAMES iconv libiconv libiconv-2 c)
+find_library(ICONV_LIBRARIES NAMES iconv libiconv libiconv-2 c)
 
+cmake_reset_check_state()
 set(CMAKE_REQUIRED_FLAGS "-fno-permissive")
 set(CMAKE_REQUIRED_INCLUDES ${ICONV_INCLUDE_DIR})
 set(CMAKE_REQUIRED_LIBRARIES ${ICONV_LIBRARIES})
-IF(ICONV_INCLUDE_DIR AND ICONV_LIBRARIES)
-  check_cxx_source_compiles("
-  #include <iconv.h>
-  int main(){
-    iconv_t conv = 0;
-    const char* in = 0;
-    size_t ilen = 0;
-    char* out = 0;
-    size_t olen = 0;
-    iconv(conv, &in, &ilen, &out, &olen);
-    return 0;
-  }
-" ICONV_SECOND_ARGUMENT_IS_CONST )
-ENDIF()
-set(CMAKE_REQUIRED_INCLUDES)
-set(CMAKE_REQUIRED_LIBRARIES)
+if(ICONV_INCLUDE_DIR AND ICONV_LIBRARIES)
+    check_cxx_source_compiles("
+    #include <iconv.h>
+    int main(){
+        iconv_t conv = 0;
+        const char* in = 0;
+        size_t ilen = 0;
+        char* out = 0;
+        size_t olen = 0;
+        iconv(conv, &in, &ilen, &out, &olen);
+        return 0;
+    }
+    " ICONV_SECOND_ARGUMENT_IS_CONST )
+endif()
+cmake_reset_check_state()
 
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(Iconv
-  REQUIRED_VARS ICONV_INCLUDE_DIR ICONV_LIBRARIES
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Iconv
+    REQUIRED_VARS ICONV_INCLUDE_DIR ICONV_LIBRARIES
 )
 
-MARK_AS_ADVANCED(
+mark_as_advanced(
   ICONV_INCLUDE_DIR
   ICONV_LIBRARIES
   ICONV_SECOND_ARGUMENT_IS_CONST
