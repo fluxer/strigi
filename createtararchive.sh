09458f48452f9c4e7971ed605ef15476c5f4ecc4
Ivailo Monev <xakepa10@gmail.com>
2021-06-29 14:51:10 +0300

    bump version
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- createtararchive.sh
+++ createtararchive.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 # script to create a release tarball for strigi
 # this is needed since the project is split up in separate git moduls
-TAG=0.8.0
+TAG=0.9.0
 git archive HEAD --format=tar --prefix=strigi-$TAG/ | xz > strigi-$TAG.tar.xz
 
