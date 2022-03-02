a563223b2d015fee0a0fc2bc6c12f098bee13ee3
Ivailo Monev <xakepa10@gmail.com>
2022-03-02 22:18:45 +0200

    replace use of system regex with std::regex
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- strigiutils/bin/deepfind/grepindexwriter.cpp
+++ strigiutils/bin/deepfind/grepindexwriter.cpp
@@ -20,29 +20,38 @@
 #include "grepindexwriter.h"
 #include <strigi/analysisresult.h>
 #include <strigi/fieldtypes.h>
-#include <regex.h>
+
+#include <regex>
 
 class GrepIndexWriter::Private
 {
 public:
     Private() {}
-    regex_t regex;
+    std::regex indexregex;
 };
 
 GrepIndexWriter::GrepIndexWriter(const char* re)
   : d(new Private()) {
-    regcomp(&d->regex, re, REG_NOSUB);
+    try {
+        d->indexregex = std::regex(re, std::regex_constants::optimize);
+    } catch (const std::regex_error &err) {
+        fprintf(stderr, "%s\n", err.what());
+    } catch (...) {
+        fprintf(stderr, "Exception caught during regex creation\n");
+    }
 }
 GrepIndexWriter::~GrepIndexWriter() {
-    regfree(&d->regex);
     delete d;
 }
+
 void
 GrepIndexWriter::startAnalysis(const Strigi::AnalysisResult* idx) {
 }
+
 void
 GrepIndexWriter::finishAnalysis(const Strigi::AnalysisResult* idx) {
 }
+
 void
 GrepIndexWriter::addText(const Strigi::AnalysisResult* idx, const char* text,
         int32_t length) {
@@ -57,7 +66,7 @@ GrepIndexWriter::addText(const Strigi::AnalysisResult* idx, const char* text,
         // look at each line separately
         if (*p == '\n' || *p == '\r') {
             s.assign(start, p-start);
-            if (regexec(&d->regex, s.c_str(), 0, 0, 0) == 0) {
+            if (std::regex_match(s.c_str(), d->indexregex) == true) {
                 printf("%s:%s\n", idx->path().c_str(), s.c_str());
             }
             start = p+1;
@@ -65,18 +74,20 @@ GrepIndexWriter::addText(const Strigi::AnalysisResult* idx, const char* text,
         p++;
     }
     s.assign(start, p-start);
-    if (regexec(&d->regex, s.c_str(), 0, 0, 0) == 0) {
+    if (std::regex_match(s.c_str(), d->indexregex) == true) {
         printf("%s:%s\n", idx->path().c_str(), s.c_str());
     }
 }
+
 void
 GrepIndexWriter::addValue(const Strigi::AnalysisResult* idx,
             const Strigi::RegisteredField* field, const std::string& value) {
-    if (regexec(&d->regex, value.c_str(), 0, 0, 0) == 0) {
+    if (std::regex_match(value.c_str(), d->indexregex) == true) {
         printf("%s:%s:%s\n", idx->path().c_str(),
             field->key().c_str(), value.c_str());
     }
 }
+
 void
 GrepIndexWriter::addValue(const Strigi::AnalysisResult* idx,
             const Strigi::RegisteredField* field,
